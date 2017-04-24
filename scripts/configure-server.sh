d7c220d19f8a4e7f6830d9f42129cc8063fae60c
ashay-maheshwari <ashay@vlabs.ac.in>
2017-04-24 09:49:08 +0530

    add scripts

--- scripts/configure-server.sh
+++ scripts/configure-server.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
-#This script will configure elasticsearch to listen remote hosts on specific port
-#Configuration is to be done in a file /etc/elasticsearch/elasticsearch.yml
-#Add host entry and port entry as given 
-#host: 0.0.0.0
-#port: 9200
+#This script will configure the following -
+# 1. Copy html codes and pages to /usr/share/nginx/www/ 
+# 2. Copy nginx configuration to /etc/nginx/sites-enabled/default
+# 3. Restart nginx configiuration
 
-
-echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
-echo "http.port: 9200" >> /etc/elasticsearch/elasticsearch.yml
-
-#Restart elasticsearch services
-echo `sudo service elasticsearch restart`
-
-#Save nginx configuration to /etc/nginx/sites-enabled/default and restart nginx
-echo `sudo cp elasticsearch-nginx.conf /etc/nginx/sites-enabled/default`
-echo `sudo service nginx restart`
-
-
-#Create an elasticsearch index named vlabs and then disable string analyzer in it
-#curl -XPUT http://vlabs-analytics.vlabs.ac.in:9200/<index-name> -d '{"index" : { "analysis" : { "analyzer" : { "default" : { "type" : "keyword" } } } } }'
-curl -XPOST http://localhost:9200/vlabs -d '{"index" : { "analysis" : { "analyzer" : { "default" : { "type" : "keyword" } } } } }'
-curl -XPOST http://localhost:9200/feedback -d '{"index" : { "analysis" : { "analyzer" : { "default" : { "type" : "keyword" } } } } }'
 
